f66769c22051323b505026f1fad83bc89eb4a5a0
Liran Tal <liran.tal@gmail.com>
2024-10-09 09:50:11 +0300

    feat: apply style for iframe embedding
    Signed-off-by: Liran Tal <liran.tal@gmail.com>

--- app.vue
+++ app.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="min-h-screen flex flex-col bg-gray-950">
+  <div class="min-h-screen flex flex-col">
     <NavigationBar v-if="!isEmbedded" />
-    <div class="flex-grow flex items-center justify-center">
+    <div
+      class="flex items-center justify-center"
+      :class="{ 'flex-grow': !isEmbedded }"
+    >
       <GameContainer />
     </div>
   </div>
 </template>
 
 <script setup>
-import NavigationBar from './components/NavigationBar.vue';
-import GameContainer from './components/GameContainer.vue';
+import NavigationBar from "./components/NavigationBar.vue";
+import GameContainer from "./components/GameContainer.vue";
 
 const route = useRoute();
 const isEmbedded = route.query.embedded !== undefined;
 </script>
+
+<style>
+body {
+  background: none transparent;
+}
+</style>
